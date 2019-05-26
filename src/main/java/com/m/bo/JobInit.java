f5fca8a0c37f4484b12aa2e5e1b36815632d14e2
xiaomiao <xingwang.miao@siebre.com>
2019-05-26 15:04:12 +0800

    quartz

---
+++ src/main/java/com/m/bo/JobInit.java
@@ -0,0 +1,50 @@
+package com.m.bo;
+
+import com.m.entity.JobEntity;
+import com.m.service.DynamicJobService;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.quartz.impl.matchers.GroupMatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+/**
+ * 容器启动后执行，相当于初始化数据
+ */
+@Component
+@Slf4j
+public class JobInit implements CommandLineRunner {
+    @Autowired
+    private SchedulerFactoryBean schedulerFactoryBean;
+    @Autowired
+    private DynamicJobService jobService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        synchronized (log) {                                                         //只允许一个线程进入操作
+            Scheduler scheduler = schedulerFactoryBean.getScheduler();
+            Set<JobKey> set = scheduler.getJobKeys(GroupMatcher.anyGroup());
+            scheduler.pauseJobs(GroupMatcher.anyGroup());                               //暂停所有JOB
+            for (JobKey jobKey : set) {                                                 //删除从数据库中注册的所有JOB
+                scheduler.unscheduleJob(TriggerKey.triggerKey(jobKey.getName(), jobKey.getGroup()));
+                scheduler.deleteJob(jobKey);
+            }
+            for (JobEntity job : jobService.loadJobs()) {                               //从数据库中注册的所有JOB
+                log.info("Job register name : {} , group : {} , cron : {}", job.getName(), job.getJobGroup(), job.getCron());
+                JobDataMap map = jobService.getJobDataMap(job);
+                JobKey jobKey = jobService.getJobKey(job);
+                if (job.getStatus().equals("OPEN")) {
+                    JobDetail jobDetail = jobService.getJobDetail(jobKey, job.getDescription(), map);
+                    scheduler.scheduleJob(jobDetail, jobService.getTrigger(job));
+                } else {
+                    log.info("Job jump name : {} , Because {} status is {}", job.getName(), job.getName(), job.getStatus());
+                }
+
+            }
+        }
+    }
+}
---
+++ src/main/java/com/m/job/DynamicJob.java
@@ -0,0 +1,61 @@
+package com.m.job;
+
+import org.quartz.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+/**
+ * Created by EalenXie on 2018/6/4 14:29
+ * :@DisallowConcurrentExecution : 此标记用在实现Job的类上面,意思是不允许并发执行.
+ * :注意org.quartz.threadPool.threadCount线程池中线程的数量至少要多个,否则@DisallowConcurrentExecution不生效
+ * :假如Job的设置时间间隔为3秒,但Job执行时间是5秒,设置@DisallowConcurrentExecution以后程序会等任务执行完毕以后再去执行,否则会在3秒时再启用新的线程执行
+ */
+@DisallowConcurrentExecution
+@Component
+public class DynamicJob implements Job {
+
+    private final static Logger log = LoggerFactory.getLogger(DynamicJob.class);
+
+    /**
+     * 核心方法,Quartz Job真正的执行逻辑.
+     *
+     * @param executorContext executorContext JobExecutionContext中封装有Quartz运行所需要的所有信息
+     * @throws JobExecutionException execute()方法只允许抛出JobExecutionException异常
+     */
+    @Override
+    public void execute(JobExecutionContext executorContext) throws JobExecutionException {
+        log.info("**************************111111111111111111111");
+        //JobDetail中的JobDataMap是共用的,从getMergedJobDataMap获取的JobDataMap是全新的对象
+        JobDataMap map = executorContext.getMergedJobDataMap();
+        String jarPath = map.getString("jarPath");
+        String parameter = map.getString("parameter");
+        log.info("Running Job name : {} ", map.getString("name"));
+        log.info("Running Job description : {}", map.getString("jobDescription"));
+        log.info("Running Job group: {} ", map.getString("group"));
+        log.info(String.format("Running Job cron : %s", map.getString("cronExpression")));
+        log.info("Running Job jar path : {} ", jarPath);
+        log.info("Running Job parameter : {} ", parameter);
+        long startTime = System.currentTimeMillis();
+
+        long endTime = System.currentTimeMillis();
+        log.info(">>>>>>>>>>>>> Running Job has been completed , cost time : {}ms\n ", (endTime - startTime));
+    }
+
+    //记录Job执行内容
+    private void logProcess(InputStream inputStream, InputStream errorStream) throws IOException {
+        String inputLine;
+        String errorLine;
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+        BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorStream));
+        while (Objects.nonNull(inputLine = inputReader.readLine())) log.info(inputLine);
+        while (Objects.nonNull(errorLine = errorReader.readLine())) log.error(errorLine);
+    }
+
+}
